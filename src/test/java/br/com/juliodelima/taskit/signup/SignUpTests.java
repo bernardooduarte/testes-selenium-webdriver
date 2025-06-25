bfb9d9e564c22fe4121e11e017c3fe6dd937723b
bernardo.o.dev <bernardo.o.dev@NXA3XxmYBX0My43.radixengrj.matriz>
2025-06-25 10:37:46 -0300

    indo para a metade do curso

---
+++ src/test/java/br/com/juliodelima/taskit/signup/SignUpTests.java
@@ -0,0 +1,7 @@
+package br.com.juliodelima.taskit.signup;
+
+import org.junit.jupiter.api.DisplayName;
+
+@DisplayName("Testes Automatizados da Funcionalidade de Sign Up")
+public class SignUpTests {
+}
